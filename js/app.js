163caf7f1ee79f50c661be243658b83de4beab27
Teemu Kinnunen <teemu.kinnunen@ovi.com>
2014-11-19 14:42:19 +0200

    Added some js script to test if they work..

---
+++ js/app.js
@@ -0,0 +1,36 @@
+var app = angular.module('feedbackApp', []);
+
+app.controller('MethodsController', function($http) {
+/* List all methods here (if shiny json does not work :) */
+this.methods =
+/*
+[
+    {
+        "name" : "End of the course feedback",
+        "description" : "Insert a short description",
+        "size" : "medium or large"
+    },
+    {
+        "name" : "Student background questionary",
+        "description" : "Insert a short description",
+        "size" : "medium or large"
+    },
+    {
+        "name" : "Student messanger",
+        "description" : "Insert a short description",
+        "size" : "any"
+    },
+    {
+        "name" : "Post-it notes",
+        "description" : "Insert a short description",
+        "size" : "small"
+    }
+]; */
+    /* works only in internet? */
+    $http.get('http://teemukinnunen.github.io/feedbackmethods/feedbackmethods.json').success(function(data) {
+      $this.methods = data;
+    });
+    
+	
+});
+
