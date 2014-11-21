5461118654a61f8a843fda3ca41a7ac346b3ddb3
Teemu Kinnunen <teemu.kinnunen@ovi.com>
2014-11-21 10:07:12 +0200

    Added functionality to load tags and method descriptions from json

--- js/app.js
+++ js/app.js
@@ -1,36 +1,56 @@
 var app = angular.module('feedbackApp', []);
 
-app.controller('MethodsController', function($http) {
-/* List all methods here (if shiny json does not work :) */
-this.methods =
-/*
-[
-    {
-        "name" : "End of the course feedback",
-        "description" : "Insert a short description",
-        "size" : "medium or large"
-    },
-    {
-        "name" : "Student background questionary",
-        "description" : "Insert a short description",
-        "size" : "medium or large"
-    },
-    {
-        "name" : "Student messanger",
-        "description" : "Insert a short description",
-        "size" : "any"
-    },
-    {
-        "name" : "Post-it notes",
-        "description" : "Insert a short description",
-        "size" : "small"
-    }
-]; */
-    /* works only in internet? */
-    $http.get('http://teemukinnunen.github.io/feedbackmethods/feedbackmethods.json').success(function(data) {
-      this.methods = data;
+app.controller("MethodsCtrl", function($scope, $http) {
+    this.methods = [];
+  $http.get('data/feedbackmethods.json').
+    success(function(data, status, headers, config) {
+      $scope.methods = data;
+    }).
+    error(function(data, status, headers, config) {
+      // log error
     });
-    
-	
+})
+
+app.directive('methodDescription', function() {
+    return {
+        templateUrl : 'method-description.html'
+    };
 });
 
+app.controller("TagFilterCtrl", function($scope, $http) {
+
+    $scope.tagsEnabled = [];
+
+    this.enableFilter = function(tagName) {
+        $scope.tagsEnabled.push(tagName);
+        for(i=0; i < $scope.tags.length; i++)
+        {
+                if($scope.tags[i].Name == tagName)
+                {
+                    if( $scope.tags[i].enabled == 0)
+                    {
+                        $scope.tags[i].enabled = 1;
+                    }
+                    else
+                    {
+                        $scope.tags[i].enabled = 0;
+                    }
+                    
+                }
+        }
+        
+    }
+
+    $http.get('data/tags.json').
+        success(function(data, status, header, config) {
+            $scope.tags = data;
+            for(i=0; i < $scope.tags.length; i++)
+            {
+                $scope.tags[i].enabled = 0;
+            }
+    }).
+        error(function(data, status, header, config) {
+            // log error
+    });
+
+})
