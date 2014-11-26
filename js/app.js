e2038f01f5b3f3677d570d96690dc86637a7dd72
Teemu Kinnunen <teemu.kinnunen@ovi.com>
2014-11-26 23:53:15 +0200

    Added functionality to filter out feedback methods based on tags

--- js/app.js
+++ js/app.js
@@ -5,6 +5,10 @@ app.controller("MethodsCtrl", function($scope, $http) {
   $http.get('data/feedbackmethods.json').
     success(function(data, status, headers, config) {
       $scope.methods = data;
+      for(i=0; i<$scope.methods.length; i++)
+      {
+        $scope.methods[i].hidden = 0;
+      }
     }).
     error(function(data, status, headers, config) {
       // log error
@@ -30,10 +34,43 @@ app.controller("TagFilterCtrl", function($scope, $http) {
                     if( $scope.tags[i].enabled == 0)
                     {
                         $scope.tags[i].enabled = 1;
+                        /* Switch off all the methods not having tags[i] */
+                        for(j=0; j < $scope.methods.length; j++)
+                        {
+                            m = $scope.methods[j];
+                            if(m[tagName] == 0)
+                            {
+                                $scope.methods[j].hidden = 1;
+                            }
+                        }
                     }
                     else
                     {
+                        /* Set tag to enabled (changes colour) */
                         $scope.tags[i].enabled = 0;
+                        
+                        /* TODO: Enable disabled methods */
+                        for(j=0; j < $scope.methods.length; j++)
+                        {
+                            /* Check all the hidden methods that if can unhide them */
+                            if($scope.methods[j].hidden)
+                            {
+                                /* Unhide the method and then check if it needs to be hidden */
+                                $scope.methods[j].hidden = 0;
+                                /* Check all the enabled tags */
+                                for(k=0; k<$scope.tags.length; k++)
+                                {
+                                    if($scope.tags.enabled)
+                                    {
+                                        if($scope.methods[j][$scope.tags[k]] == 0)
+                                        {
+                                            $scope.methods[j].hidden = 1;
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                        
                     }
                     
                 }
